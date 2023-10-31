61834bcc31afe23073587187e7af68c916f17063
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2023-10-31 14:02:53 +0600

    picture added

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,17 +12,37 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+        body: SingleChildScrollView(
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("LogIn Page"),
-            SizedBox(height: 50.0,),
+            SizedBox(
+              height: 20.0,
+            ),
+            SizedBox(
+              height: 190.0,
+              width: 250,
+              // child: Padding(
+              //   padding: EdgeInsets.all(50.0), // You can adjust the padding values as per your needs
+              child: Image.asset("assets/Easy.jpg"),
+              // ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text("Login Page"),
+            SizedBox(
+              height: 20.0,
+            ),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Email',
                 border: OutlineInputBorder(),
               ),
+            ),
+            SizedBox(
+              height: 15.0,
             ),
             TextField(
               decoration: InputDecoration(
@@ -30,11 +50,38 @@ class _MainScreenState extends State<MainScreen> {
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 20.0,),
-            ElevatedButton(onPressed: (){},
-                child: Text(
-                  "Submit"
-                ),
+            SizedBox(
+              height: 10.0,
+            ),
+            // Container(
+            //   width: 50, // Set the width and height to make it circular
+            //   height: 50,
+            //   decoration: BoxDecoration(
+            //     shape: BoxShape.circle, // Shape it into a circle
+            //     color: Colors.white, // Background color of the circle
+            //   ),
+            //   child: Material(
+            //     color: Colors.transparent, // Set the material color to transparent
+            //     child: InkWell(
+            //       onTap: () {
+            //         // Add your button tap functionality here
+            //       },
+            //       child: Center(
+            //         child: Text(
+            //           'Login',
+            //           style: TextStyle(
+            //             color: Colors.black, // Text color
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Login",
+              ),
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
               ),
@@ -42,6 +89,6 @@ class _MainScreenState extends State<MainScreen> {
           ],
         ),
       ),
-    );
+    ));
   }
 }
